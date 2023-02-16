b3f547cf1d6d4e45846162adbd8cd3109c0c85f9
Nate <14845347+garlic-os@users.noreply.github.com>
2023-02-16 15:49:00 -0600

    Load CORS proxy too

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,3 +1,5 @@
-// Start the database and Discord bot immediately.
+// Initialize these modules immediately; do not wait for the web app to
+// lazy load them.
 import "$lib/server/database";
 import "$lib/server/bot";
+import "$lib/server/cors-proxy";
