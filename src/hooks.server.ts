b09a6b649471f83f1595843afbc8c522e56e3b5b
Nate <14845347+garlic-os@users.noreply.github.com>
2023-02-17 15:07:33 -0600

    Actually only the bot needs to start immediately

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,2 @@
-// Initialize these modules immediately; do not wait for the web app to
-// lazy load them.
-import "$lib/server/database";
+// Start the bot immediately; do not wait for the web app to lazy load it.
 import "$lib/server/bot";
-import "$lib/server/cors-proxy";
