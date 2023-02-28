ecf05349d153c924154473fa92f255805d3bc75a
Nate <14845347+garlic-os@users.noreply.github.com>
2023-02-28 12:30:34 -0600

    Fix it
    Didn't split chunks properly when they didn't fit into the buffer before, now it does

--- src/lib/server/set-size-chunk-stream.ts
+++ src/lib/server/set-size-chunk-stream.ts
@@ -9,22 +9,37 @@ export default class SetSizeChunkStream extends TransformStream<Uint8Array, Uint
 		super({
 			transform: (chunk, controller) => {
 				// Copy the chunk into the buffer.
-				this.buffer.set(chunk, this.offset);
-				this.offset += chunk.byteLength;
-
-				// If the buffer is full, send it to the next stream.
-				if (this.offset === size) {
-					controller.enqueue(this.buffer);
-					this.buffer = new Uint8Array(size);
-					this.offset = 0;
-				}
+				// If the chunk is too big to fit, copy in what fits, send out
+				// the buffer, then loop to copy the rest.
+				let dataRemaining = chunk.byteLength;
+				do {
+					const spaceLeft = size - this.offset;
+					this.buffer.set(
+						chunk.subarray(
+							0,
+							Math.min(chunk.byteLength, spaceLeft)
+						),
+						this.offset
+					);
+					dataRemaining = Math.max(chunk.byteLength - spaceLeft, 0);
+					if (chunk.byteLength >= spaceLeft) {
+						// Buffer is now full, so send it out.
+						controller.enqueue(this.buffer);
+						this.buffer = new Uint8Array(size);
+						this.offset = 0;
+						chunk = chunk.subarray(spaceLeft);
+					} else {
+						this.offset += chunk.byteLength;
+					}
+				} while (dataRemaining > 0);
 			},
+
 			flush: (controller) => {
-				// Send the remaining data to the next stream.
+				// Send out any remaining data.
 				if (this.offset > 0) {
 					controller.enqueue(this.buffer.subarray(0, this.offset));
 				}
-			},
+			}
 		});
 
 		this.buffer = new Uint8Array(size);
