6f48f6e2edb7f630eb3ecbbdc6bb40cb0583a5da
Nate <14845347+garlic-os@users.noreply.github.com>
2023-03-08 09:21:53 -0600

    Fix autocomplete handling

--- src/lib/server/bot/index.ts
+++ src/lib/server/bot/index.ts
@@ -44,22 +44,38 @@ bot.on(Discord.Events.ClientReady, async () => {
 
 
 bot.on(Discord.Events.InteractionCreate, async (interaction) => {
-	if (!interaction.isChatInputCommand()) return;
-	const command = bot.commands.get(interaction.commandName);
+	if (interaction.isChatInputCommand()) {
+		const command = bot.commands.get(interaction.commandName);
 
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
+		if (!command) {
+			console.error(`No command matching ${interaction.commandName} was found.`);
+			return;
+		}
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({
-			content: "There was an error while executing this command!",
-			ephemeral: true
-		});
+		try {
+			await command.execute(interaction);
+		} catch (error) {
+			console.error(error);
+			await interaction.reply({
+				content: "There was an error while executing this command!",
+				ephemeral: true
+			});
+		}
+	} else if (interaction.isAutocomplete()) {
+		const command = bot.commands.get(
+			interaction.commandName
+		) as AutocompleteCommandHandler | undefined;
+
+		if (!command) {
+			console.error(`No command matching ${interaction.commandName} was found.`);
+			return;
+		}
+
+		try {
+			await command.autocomplete(interaction);
+		} catch (error) {
+			console.error(error);
+		}
 	}
 });
 
