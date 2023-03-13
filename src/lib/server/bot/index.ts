40e5d0fbf8eac0eb4adf3efee52278fa1c831005
Nate Kean <14845347+garlic-os@users.noreply.github.com>
2023-03-13 13:01:22 -0500

    Refactor

--- src/lib/server/bot/index.ts
+++ src/lib/server/bot/index.ts
@@ -9,17 +9,10 @@ export async function getUploadChannel(): Promise<Discord.TextChannel> {
 
 
 export async function uploadToDiscord(buffer: Buffer, filename: string): Promise<string> {
-	// Upload the file to Discord.
 	const attachment = new Discord.AttachmentBuilder(buffer, {
 		name: filename,
 	});
 	const uploadChannel = await getUploadChannel();
-	const message = await uploadChannel.send({files: [attachment]});
-
-	// Return the URL of the uploaded file.
-	const sentAttachment = message.attachments.first();
-	if (!sentAttachment) {
-		throw new Error("No attachment found in message");
-	}
-	return sentAttachment.url;
+	const sentMessage = await uploadChannel.send({ files: [attachment] });
+	return sentMessage.attachments.first()!.url;
 }
