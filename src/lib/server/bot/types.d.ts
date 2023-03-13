3bcad6a469adaa305dd1abfac5f917b3cf6603c4
Nate Kean <14845347+garlic-os@users.noreply.github.com>
2023-03-13 13:00:25 -0500

    Rename types.ts to types.d.ts

--- src/lib/server/bot/types.d.ts
+++ src/lib/server/bot/types.d.ts
@@ -1,5 +1,3 @@
-import type Discord from "discord.js";
-
 interface SlashCommandHandler {
 	data: Discord.SlashCommandBuilder;
 	execute(interaction: Discord.CommandInteraction): Promise<void>;
@@ -7,24 +5,4 @@ interface SlashCommandHandler {
 
 interface AutocompleteCommandHandler extends SlashCommandHandler {
 	autocomplete(interaction: Discord.AutocompleteInteraction): Promise<void>;
-}
-
-
-abstract class ISplitter extends Discord.Client {
-	public static readonly isSplitter: true = true;  // Sentinel for type checking
-	protected commands: Discord.Collection<string, SlashCommandHandler>;
-	protected uploadChannel: Discord.TextChannel | null;
-	protected ready: Promise<void>;
-	protected abstract onClientReady(): Promise<void>;
-	protected abstract onInteractionCreate(interaction: Discord.Interaction): Promise<void>;
-	public abstract getUploadChannel(): Promise<Discord.TextChannel>;
-
-	constructor(options: Discord.ClientOptions) {
-		super(options);
-		this.commands = new Discord.Collection();
-		this.uploadChannel = null;
-		this.ready = new Promise((resolve) => {
-			this.once(Discord.Events.ClientReady, () => resolve());
-		});
-	}
 }
