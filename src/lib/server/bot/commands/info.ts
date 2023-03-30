6ce578cac32694aadbcfaec61c5ae7c959a39896
Nate Kean <14845347+garlic-os@users.noreply.github.com>
2023-03-29 23:07:50 -0500

    Update info.ts

--- src/lib/server/bot/commands/info.ts
+++ src/lib/server/bot/commands/info.ts
@@ -17,7 +17,7 @@ export async function execute(interaction: Discord.ChatInputCommandInteraction):
                     iconURL: "https://i.imgur.com/QQXKVSY.png",  // My Discord avatar
                     url: "https://github.com/garlic-os/"
                 })
-                .setDescription("Splitter is a Discord bot that allows you to split files into smaller chunks, and then recombine them later. It's useful for sharing large files, or for sending files that are too large to send in a single message.\n[GitHub](https://github.com/garlic-os/Splitter)")
+                .setDescription("Splitter is a Discord bot that allows you to split files into smaller chunks and recombine them later. It's meant to make sharing large files in Discord as seamless as possible.\n[GitHub](https://github.com/garlic-os/Splitter)")
                 .setColor(Colors.blue)
                 .setThumbnail(interaction.client.user.displayAvatarURL())
         ]
