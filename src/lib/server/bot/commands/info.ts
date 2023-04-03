3617e8d01155dcfad5a950d57403343a0b53c486
Nate <14845347+garlic-os@users.noreply.github.com>
2023-04-03 09:10:12 -0500

    Use tabs

--- src/lib/server/bot/commands/info.ts
+++ src/lib/server/bot/commands/info.ts
@@ -7,18 +7,18 @@ export const data = new Discord.SlashCommandBuilder()
 
 
 export async function execute(interaction: Discord.ChatInputCommandInteraction): Promise<void> {
-    interaction.reply({
-        embeds: [
-            new Discord.EmbedBuilder()
-                .setTitle("Splitter")
-                .setAuthor({
-                    name: "A garlicSoft® product",
-                    iconURL: "https://i.imgur.com/QQXKVSY.png",  // My Discord avatar
-                    url: "https://github.com/garlic-os/"
-                })
-                .setDescription("Splitter is a Discord bot that allows you to split files into smaller chunks, and then recombine them later. It's useful for sharing large files, or for sending files that are too large to send in a single message.\n[GitHub](https://github.com/garlic-os/Splitter)")
-                .setColor(Discord.Colors.Blue)
-                .setThumbnail(interaction.client.user.displayAvatarURL())
-        ]
-    });
+	interaction.reply({
+		embeds: [
+			new Discord.EmbedBuilder()
+				.setTitle("Splitter")
+				.setAuthor({
+					name: "A garlicSoft® product",
+					iconURL: "https://i.imgur.com/QQXKVSY.png",  // My Discord avatar
+					url: "https://github.com/garlic-os/"
+				})
+				.setDescription("Splitter is a Discord bot that allows you to split files into smaller chunks, and then recombine them later. It's useful for sharing large files, or for sending files that are too large to send in a single message.\n[GitHub](https://github.com/garlic-os/Splitter)")
+				.setColor(Discord.Colors.Blue)
+				.setThumbnail(interaction.client.user.displayAvatarURL())
+		]
+	});
 }
