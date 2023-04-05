55d75f36aa46421d1269f4e325cb26c8d9b32ea2
Nate Kean <14845347+garlic-os@users.noreply.github.com>
2023-04-05 09:59:11 -0500

    Revert "Add HTTPS support"
    Actually, I want to put this in its own branch; I haven't tested it yet.
    
    This reverts commit 19063c674116367a9cd29b736b55dc568ac26f36.

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,6 @@
-import fs from "node:fs";
 import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig, type UserConfigExport } from "vite";
-import * as Config from "./config";
+import { defineConfig } from "vite";
 
-
-const viteConfig: UserConfigExport = {
-	plugins: [sveltekit()],
-};
-
-
-if (Config.sslKeyPath && Config.sslCertPath) {
-	viteConfig.server = {
-		https: {
-			key: fs.readFileSync(Config.sslKeyPath),
-			cert: fs.readFileSync(Config.sslCertPath),
-		},
-	};
-}
-
-
-export default defineConfig(viteConfig);
+export default defineConfig({
+	plugins: [sveltekit()]
+});
