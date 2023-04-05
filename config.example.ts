a1956639830790957a17dabd5926a0c747199bb6
Nate <14845347+garlic-os@users.noreply.github.com>
2023-04-05 09:49:01 -0500

    Fix typo
    Oops! I had this in my config.ts for a while but forgot to put it in the config.example.ts

--- config.example.ts
+++ config.example.ts
@@ -5,7 +5,7 @@ export const uploadTokenLifespan: number = 1000 * 60 * 60 * 24;  // 24 hours
 export const partSize: number = 1024 * 1024 * 8; // 8 MB
 export const fileSizeLimit: number = 1024 * 1024 * 1024;  // 1 GB
 
-const corsProxyPort: number = 3001;
+export const corsProxyPort: number = 3001;
 export const corsProxyURL: string = `http://localhost:${corsProxyPort}`;
 
 export const discordBotToken: string = "";
