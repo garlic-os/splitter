19063c674116367a9cd29b736b55dc568ac26f36
Nate <14845347+garlic-os@users.noreply.github.com>
2023-04-05 09:50:15 -0500

    Add HTTPS support
    Just key/cert support right now. If you use the pbx/passphrase or whatever it is, figure it out!

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,22 @@
+import fs from "node:fs";
 import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
+import { defineConfig, type UserConfigExport } from "vite";
+import * as Config from "./config";
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+
+const viteConfig: UserConfigExport = {
+	plugins: [sveltekit()],
+};
+
+
+if (Config.sslKeyPath && Config.sslCertPath) {
+	viteConfig.server = {
+		https: {
+			key: fs.readFileSync(Config.sslKeyPath),
+			cert: fs.readFileSync(Config.sslCertPath),
+		},
+	};
+}
+
+
+export default defineConfig(viteConfig);
