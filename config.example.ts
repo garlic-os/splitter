83e515d441268690ce315672ede7f157ad0917b2
Nate <14845347+garlic-os@users.noreply.github.com>
2023-04-11 10:38:25 -0500

    Refactor

--- config.example.ts
+++ config.example.ts
@@ -1,12 +1,13 @@
 export const webappPort: number = 5173;
 export const webappURL: string = `http://localhost:${webappPort}`;
+export const corsProxyPort: number = 3001;
+export const corsProxyURL: string = `http://localhost:${corsProxyPort}`;
+
 export const databasePath: string = "./database.db";
+
 export const uploadTokenLifespan: number = 1000 * 60 * 60 * 24;  // 24 hours
 export const partSize: number = 1024 * 1024 * 8;  // 8 MB
 export const fileSizeLimit: number = 1024 * 1024 * 1024;  // 1 GB
-
-export const corsProxyPort: number = 3001;
-export const corsProxyURL: string = `http://localhost:${corsProxyPort}`;
 
 export const discordBotToken: string = "";
 export const discordClientID: string = "";
