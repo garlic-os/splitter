a31db6cbc3b0d8f6721e10cae947caec6165b408
Nate <14845347+garlic-os@users.noreply.github.com>
2023-04-17 10:37:04 -0500

    Raise default file size limit to 10 GB

--- config.example.ts
+++ config.example.ts
@@ -4,8 +4,8 @@ export const webappURL: string = `http://localhost:${webappPort}`;
 export const databasePath: string = "./database.db";
 
 export const uploadTokenLifespan: number = 1000 * 60 * 60 * 24;  // 24 hours
-export const fileSizeLimit: number = 1024 * 1024 * 1024;  // 1 GB
 export const partSize: number = 1024 * 1024 * 25;  // 25 MB - Make this the max attachment size in the server your instance uploads to.
+export const fileSizeLimit: number = 1024 * 1024 * 1024 * 10;  // 10 GB
 
 export const discordBotToken: string = "";
 export const discordClientID: string = "";
