bbdd5fc21b739fac3f9955510e4630fb6d7886ba
Nate <14845347+garlic-os@users.noreply.github.com>
2023-09-29 01:17:33 -0300

    Remove unused parameter

--- src/routes/upload/+page.server.ts
+++ src/routes/upload/+page.server.ts
@@ -2,6 +2,6 @@ import type { PageServerLoad } from "./$types";
 import { error } from "@sveltejs/kit";
 import StatusCodes from "http-status-codes";
 
-export const load = ((/*{ params }*/) => {
+export const load = (() => {
 	throw error(StatusCodes.UNAUTHORIZED, "No upload token provided");
 }) satisfies PageServerLoad;
